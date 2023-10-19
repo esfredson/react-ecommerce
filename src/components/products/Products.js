0020f689eb046b938789c0dae56032c0fbc93dcf
esfredson <federicofrasquet@gmail.com>
2023-10-19 23:06:28 +0200

    creamos el componente Products para contener todos los productos

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -2,18 +2,20 @@ import React from 'react'
 import Loader from '../layout/Loader'
 import Product from '../product/Product'
 
+//creamos el componente 'Products' para contener todos los productos
 const Products = ({products, col, loading}) => {
     if(loading)
         return <Loader />
 
-  return (
-    
+  return (  
         <React.Fragment>        
             {
                 products
-                ? products.map(productElement => (
-                    <Product key={productElement.id} product={productElement} col={col} />
-                    ))
+                ? products.map(
+                    productElement => (
+                        <Product key={productElement.id} product={productElement} col={col} />
+                    )
+                )
                 : <Loader />
             }
         </React.Fragment>
